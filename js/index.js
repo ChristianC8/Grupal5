55ae94bdae31b0d35c7036ae74362233a35c2efc
ChristianC <castellanotomas2000@outlook.com>
2022-10-13 12:48:03 -0300

    todo

--- js/index.js
+++ js/index.js
@@ -2,4 +2,18 @@
 
 function showMenu(){
     document.getElementById("smallMenuId").classList.toggle("show");
+    if (document.getElementById("smallMenuId").classList.contains("show") && !document.getElementById("opcionesDiv").classList.contains("showDiv") ){
+        document.getElementById("opcionesDiv").classList.toggle("showDiv"); 
+        document.getElementById("carrousel").style.display = "block";
+    }
+}
+
+function opciones(){
+    document.getElementById("opcionesDiv").classList.toggle("showDiv"); 
+    if(!document.getElementById("opcionesDiv").classList.contains("showDiv")){
+        document.getElementById("carrousel").style.display = "none";
+    }else{
+        document.getElementById("carrousel").style.display = "block";
+    }
+
 }
